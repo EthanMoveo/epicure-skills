0bca9efcbda6fd3687839613d9befae0931a4105
EthanMoveo <ethanf@moveo.co.il>
2024-10-14 14:54:48 +0300

    add desktop version for header

--- src/components/atoms/Header.styles.tsx
+++ src/components/atoms/Header.styles.tsx
@@ -5,7 +5,17 @@ export const HeaderContainer = styled.header`
   justify-content: space-between;
   align-items: center;
   position: relative;
-  height: 46px; 
+  height: 46px;
+
+  @media (min-width: 1440px) {
+    height: 36px;
+    margin: 14px 127px;
+  }
+`;
+
+export const LeftSection = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export const MenuButton = styled.button`
@@ -17,6 +27,10 @@ export const MenuButton = styled.button`
   margin-top: 11px;
   margin-bottom: 11px;
   margin-left: 20px;
+
+  @media (min-width: 1440px) {
+    display: none; /* Cacher le menu hamburger sur desktop */
+  }
 `;
 
 export const MenuIcon = styled.img`
@@ -24,24 +38,79 @@ export const MenuIcon = styled.img`
   height: 24px;
 `;
 
-export const Icon = styled.img`
-  width: 20px;
-  height: 20px;
-  margin: 13px 0;
-`;
-
 export const LogoIcon = styled.img`
   width: 32.93px;
   height: 32px;
-  position: absolute; 
-  left: 50%; 
-  transform: translateX(-50%); 
   margin: 7px 0;
+  
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  
+  @media (min-width: 1440px) {
+    position: static;
+    transform: none;
+    margin-left: 0;
+    margin-top: 15.5px;
+
+  }
+`;
+
+export const PlatformName = styled.span`
+  display: none;
+  margin-left: 10px; /* Espacement entre le logo et le nom de la plateforme */
+  font-size: 27px;
+  font-weight: 200;
+  font-family: 'Helvetica Neue';
+  line-height: 32.64px;
+  color: #000000;
+
+  @media (min-width: 1440px) {
+    display: inline-block; /* Afficher le nom de la plateforme sur desktop */
+        margin-top: 15.5px;
+
+  }
+`;
+
+export const NavLinks = styled.nav`
+  display: none;
+
+  @media (min-width: 1440px) {
+    display: flex;
+    gap: 30px;
+    margin-left: 20px; 
+    margin-top: 15.5px; 
+  }
+`;
+
+export const NavLinkItem = styled.a`
+  text-decoration: none;
+  color: black;
+  font-size: 18px;
+  font-weight: 200;
+  font-family: 'Helvetica Neue';
+  font-family: Helvetica Neue;
+  line-height: 21.76px;
+  letter-spacing: 1.92px;
+  margin-top: 5.5px;
 `;
 
 export const IconGroup = styled.div`
   display: flex;
   justify-content: flex-end;
-  gap: 20px;
+  gap: 32px;
   margin-right: 20px;
+      
+  @media (min-width: 1440px) {
+    margin-top: 20px;
+    margin-right: 0px;
+  }
+  
+`;
+
+export const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin: 13px 0;
+
 `;
--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-
 import Logo from './../../assets/icons/logo-epicure.svg';
 import HamburgerMenu from './../../assets/icons/hamburger-menu.svg';
 import ProfileIcon from './../../assets/icons/profile-icon.svg';
 import SearchIcon from './../../assets/icons/search-icon.svg';
 import BagIcon from './../../assets/icons/bag-icon.svg';
-import { HeaderContainer, MenuButton, MenuIcon, Icon, LogoIcon, IconGroup } from './Header.styles.tsx';
+import { HeaderContainer, LeftSection, MenuButton, MenuIcon, LogoIcon, PlatformName, NavLinks, NavLinkItem, IconGroup, Icon } from './Header.styles.tsx';
 
 const Header: React.FC = () => {
   return (
     <HeaderContainer>
-      <MenuButton>
-        <MenuIcon src={HamburgerMenu} alt="Menu" />
-      </MenuButton>
-      <LogoIcon src={Logo} alt="Logo" />
+      <LeftSection>
+        <MenuButton>
+          <MenuIcon src={HamburgerMenu} alt="Menu" />
+        </MenuButton>
+        <LogoIcon src={Logo} alt="Logo" />
+        <PlatformName>EPICURE</PlatformName>
+        <NavLinks>
+          <NavLinkItem href="#">Restaurants</NavLinkItem>
+          <NavLinkItem href="#">Chefs</NavLinkItem>
+        </NavLinks>
+      </LeftSection>
       <IconGroup>
         <Icon src={SearchIcon} alt="Search" />
         <Icon src={ProfileIcon} alt="Profile" />
