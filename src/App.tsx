253e61fa17af812c8009244e163c1a89e67507d2
EthanMoveo <ethanf@moveo.co.il>
2024-10-23 12:34:24 +0300

    Merging

--- src/App.tsx
+++ src/App.tsx
@@ -33,11 +33,9 @@ function App() {
       CardComponent={DishCard}
     />
     <SectionIcons icons={icons} />
+    {/* <ChefSection title="Chef of the week" image={chefPic} text="Chef Yossi Shitrit has been living and breathing his culinary dreams for more than two decades, including running the kitchen in his first restaurant, the fondly-remembered Violet, located in Moshav  Udim. Shitrit's creativity and culinary  acumen born of long experience  are expressed in the every detail of each and every dish." subtitle="Yossi's Restaurants" restaurants={chefRestaurants}/> */}
     <AboutUs />
-    <Footer />
-
-    
-    <ChefSection title="Chef of the week" image={chefPic} text="Chef Yossi Shitrit has been living and breathing his culinary dreams for more than two decades, including running the kitchen in his first restaurant, the fondly-remembered Violet, located in Moshav  Udim. Shitrit's creativity and culinary  acumen born of long experience  are expressed in the every detail of each and every dish." subtitle="Yossi's Restaurants" restaurants={chefRestaurants}/>
+    <Footer />    
     </>
   )
 }
